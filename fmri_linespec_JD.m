781133f9065d0ef5797a3bbb240588d0352087e9
jaduckwo <jaduckwo@ucsd.edu>
2024-09-04 21:22:46 -0700

    Added load data section

--- fmri_linespec_JD.m
+++ fmri_linespec_JD.m
@@ -4,10 +4,16 @@
 % stimulated fmri datasets. 
 
 %% Load data
-
-
-
+clear; clc; close all;
+cd('Y:\DataAnalysis\MRI\Human240904\13685568');
+dataDir = fullfile(pwd,'data');
+dataFile = 'vsmDrivenP1.mat';
+disp(['processing ',dataFile]);
+subj = extractBetween(dataFile,'Driven','.mat');
+subj = subj{1};
+load(fullfile(dataDir,dataFile))
 %% 
+
 
 
 
